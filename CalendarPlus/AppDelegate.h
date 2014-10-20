596037a97e6e1bacb2c05d5a589abc037eaf83ef
Howon Song <hs459@cornell.edu>
2014-10-19 22:25:44 -0400

    Initial Commit

---
+++ CalendarPlus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CalendarPlus
+//
+//  Created by Howon Song on 10/19/14.
+//  Copyright (c) 2014 Howon Song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CalendarPlus/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CalendarPlus
+//
+//  Created by Howon Song on 10/19/14.
+//  Copyright (c) 2014 Howon Song. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
