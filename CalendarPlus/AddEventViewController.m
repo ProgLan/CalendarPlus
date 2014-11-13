4f0f3535b9a9196265e95b8eadedf937eeebf72a
Howon Song <hs459@cornell.edu>
2014-11-13 02:06:22 -0500

    Finally got the formula to find the points that lie on the bezier curve.

--- CalendarPlus/AddEventViewController.m
+++ CalendarPlus/AddEventViewController.m
@@ -271,7 +271,7 @@ static CGPoint midPointForPoints(CGPoint p1, CGPoint p2) {
 - (IBAction)displayGestureForTapRecognizer:(UITapGestureRecognizer *)recognizer
 {
     CGPoint location = [recognizer locationInView:self.view];
-    NSLog(@"%@", NSStringFromCGPoint(location));
+    NSLog(@"Touch location: %@", NSStringFromCGPoint(location));
     
     if (self.currentGraph != nil) {
         [self.currentGraph removeFromSuperlayer];
@@ -293,61 +293,109 @@ static CGPoint midPointForPoints(CGPoint p1, CGPoint p2) {
     NSDateComponents *endDateComponents = [self.calendar components:(NSCalendarUnitDay) fromDate:self.eventEndDate];
     NSInteger endDay = [endDateComponents day];
     
-    NSLog(@"startDay: %ld", (long)startDay);
-    NSLog(@"endDay: %ld", (long)endDay);
+//    NSLog(@"startDay: %ld", (long)startDay);
+//    NSLog(@"endDay: %ld", (long)endDay);
     
     NSInteger numDatesSelected = endDay - startDay + 1;
     float tickInterval = xRange / numDatesSelected;
     
-    NSLog(@"tickInterval %f", tickInterval);
+//    NSLog(@"tickInterval %f", tickInterval);
     
     CGPoint origin = CGPointMake(xStart, 620.0);
     CGPoint endpt = CGPointMake(xEnd, 620.0);
     CGPoint midpt1 = midPointForPoints(origin, location);
     CGPoint midpt2 = midPointForPoints(location, endpt);
+    CGPoint ctrlpt1 = CGPointMake(midpt1.x, midpt1.y+50);
+    CGPoint ctrlpt2 = CGPointMake(midpt2.x, midpt2.y+50);
     
     UIBezierPath *path = [UIBezierPath bezierPath];
     [path moveToPoint:origin];
-    [path addQuadCurveToPoint:location controlPoint:CGPointMake(midpt1.x, midpt1.y+50)];
-    [path addQuadCurveToPoint:endpt controlPoint:CGPointMake(midpt2.x, midpt2.y+50)];
+    [path addQuadCurveToPoint:location controlPoint:ctrlpt1]; //ctrlpt1
+    
+    
+    [path addQuadCurveToPoint:endpt controlPoint:ctrlpt2]; //ctrlpt2
+    
+    NSLog(@"X1: %f", ctrlpt1.x);
+    NSLog(@"X2: %f", ctrlpt2.x);
+    float tVal1 = [self getTvalFromBezierPath:location.x x0Val:0.0 x1Val:ctrlpt1.x x2Val:location.x];
+    float tVal2 = [self getTvalFromBezierPath:location.x x0Val:location.x x1Val:ctrlpt2.x x2Val:endpt.x];
+    NSLog(@"tVal1: %f", tVal1);
+    NSLog(@"tVal1: %f", tVal2);
+    float yVal1 = [self getCoordFromBezierPath:tVal1 origin:0.0 p1Val:ctrlpt1.y p2Val:location.y];
+    float yVal2 = [self getCoordFromBezierPath:tVal2 origin:location.y p1Val:ctrlpt2.y p2Val:endpt.y];
+    NSLog(@"Y_real: %f", location.y);
+    NSLog(@"Y_val1: %f", yVal1);
+    NSLog(@"Y_val2: %f", yVal2);
+    
+//    NSLog(@"Y_predicted: %f", yVal); // y should equal location.y
     
     [shapeLayer setPath:path.CGPath];
     
 //    Represent the workload on the calendar
-    //Get a range of NSDate objects
     
+    //Get a range of NSDate objects
     
     NSDate *currentDate = [self.eventStartDate copy];
     NSMutableArray *selectedDates = [[NSMutableArray alloc] init];
-    [selectedDates addObject:currentDate];
-    for (int i = 1; i < numDatesSelected; i++) {
+//    [selectedDates addObject:currentDate];
+    float xPointOnBezierPath = 0.0;
+    for (int i = 0; i < numDatesSelected; i++) {
+        [selectedDates addObject:currentDate];
         NSDateComponents *aDayDiff = [[NSDateComponents alloc] init];
         aDayDiff.day = 1;
         NSDate *aDayAfter = [[NSCalendar currentCalendar] dateByAddingComponents:aDayDiff
                                                                           toDate:currentDate
                                                                          options:0];
         currentDate = aDayAfter;
-        [selectedDates addObject:currentDate];
+        
+        
+        float tmp = xPointOnBezierPath;
+        xPointOnBezierPath = xPointOnBezierPath + tickInterval;
+        float fillHeightFromTwoPts = (tmp + xPointOnBezierPath) / 2; // get the avg between prev xPoint and new xPoint
+        
+//        NSLog(@"tick x = %f", xPointOnBezierPath);
+//        NSLog(@"fillHeight h = %f", fillHeightFromTwoPts);
     }
-    NSLog(@"dates: %@", selectedDates);
+//    NSLog(@"dates: %@", selectedDates);
+    
+    UIButton *firstButton = (UIButton *)[self.smallCalendarView viewWithTag:[selectedDates[0] timeIntervalSince1970]];
+    float btnWidth = firstButton.frame.size.width;
+    float btnHeight = firstButton.frame.size.height;
     
     for (int i = 0; i < numDatesSelected; i++) {
         UIButton *myButton = (UIButton *)[self.smallCalendarView viewWithTag:[selectedDates[i] timeIntervalSince1970]];
-        float btnWidth = myButton.frame.size.width;
-        float btnHeight = (myButton.frame.size.height * 0.1 * i);
-        UIImage *img = [self imageWithColor:[UIColor greenColor] width:btnWidth height:btnHeight];
-        [myButton setBackgroundImage:img forState:(UIControlStateNormal)];
+        float fillHeight = i*5.0+1;
+        UIImage *img = [self imageWithColor:[UIColor greenColor] buttonWidth:btnWidth buttonHeight:btnHeight fillHeight:fillHeight];
+        [myButton setImage:img forState:UIControlStateNormal];
+        
+//        To keep text alive I should probably do this
+//        [testBtn setTitleEdgeInsets:UIEdgeInsetsMake(70.0, -150.0, 5.0, 5.0)];
+//        [testBtn setTitle:@"Your text" forState:UIControlStateNormal];
+        
     }
+}
+
+- (float)getTvalFromBezierPath:(float)x x0Val:(float)x0 x1Val:(float)x1 x2Val:(float)x2 {
+    NSLog(@"getTval: x: %f, x0: %f, x1: %f, x2: %f", x, x0, x1, x2);
+    float tVal = (x-x0)/(2*(x1-x0));
+    return tVal;
+}
+
+- (float)getCoordFromBezierPath:(float)t origin: (float)origin p1Val: (float)p1 p2Val: (float)p2 {
+    return (pow((1-t),2) * origin) + (2 * t * (1-t) * p1) + (pow(t,2) * p2);
 }
 
 //SmallCalendarView related
 
-- (UIImage *)imageWithColor:(UIColor *)color width:(float)w height:(float)h {
-    CGRect rect = CGRectMake(0, 0, w, h);
-    // Create a w by H pixel context
+- (UIImage *)imageWithColor:(UIColor *)color buttonWidth:(float)bw buttonHeight:(float)bh fillHeight:(float)fh {
+//    float heightOffset = btnHeight - h;
+//    NSLog(@"btn height %f", h);
+//    NSLog(@"height offset %f", heightOffset);
+    CGRect rect = CGRectMake(0, 0, bw, bh);
     UIGraphicsBeginImageContextWithOptions(rect.size, NO, 0);
+    CGRect innerRect = CGRectMake(0 , bw - fh, bw, fh);
     [color setFill];
-    UIRectFill(rect);   // Fill it with your color
+    UIRectFill(innerRect);   // Fill it with your color
     UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
     return image;
